574e56b26f0f944628bd6f3db994abd25e260f36
s.arzamastsev <sarzamastsev@cnord.ru>
2019-10-24 17:23:32 +0300

    reduce size to minimum

--- features/android/support/app_life_cycle_hooks.rb
+++ features/android/support/app_life_cycle_hooks.rb
@@ -1,17 +1,8 @@
 Before do |scenario| # Cucumber::RunningTestCase::Scenario
-  log "Check if tag @reinstall_app is present for Scenario: '#{scenario.name}'? '#{scenario.source_tag_names}'"
-  if scenario.source_tag_names.include?('@reinstall_app')
-    log 'Scenario tag @reinstall_app is present - reinstalling apps...'
-    reinstall_apps
-  else
-    log 'Scenario tag @reinstall_app is NOT present - proceeding with same apps...'
-  end
-
 #  start_test_server_in_background
 end
 
 After do |scenario| # Cucumber::RunningTestCase::Scenario
-
 #  screenshot_embed if scenario.failed?
 #  shutdown_test_server
 end
--- features/step_definitions/MyAlarm_steps.rb
+++ features/step_definitions/MyAlarm_steps.rb
@@ -9,50 +9,10 @@ Given ("Я нахожусь на основном экране") do
   end
 end
 
-Given ("Я нахожусь на экране ввода SMS-кода") do
-  macro 'Я нахожусь на экране входа'
-  macro 'Я ввожу номер телефона для авторизации'
-  macro 'Я оказываюсь на экране ввода SMS-кода'
+Given("Я выхожу из приложения") do
+  pending # Write code here that turns the phrase above into concrete actions
 end
 
-Given ("Я нахожусь на экране входа") do
-  macro 'Я нахожусь на стартовом экране'
-  macro 'Я нажимаю кнопку входа в приложение ->'
-  macro 'Я оказываюсь на экране входа'
-end
-
-Given ("Я ввожу номер телефона для авторизации") do
-  @page.enter_phone_number
-end
-
-Given ("Я оказываюсь на экране ввода SMS-кода") do
-  @page = page(SmsCodePage).await(timeout:30)
-end
-
-Given ("Я нахожусь на стартовом экране") do
-  @page = page(ProxyPage).await(timeout:30)
-  if @page.is_start_screen?
-    @page = page(StartPage).await(timeout:30)
-  elsif @page.is_main_screen?
-    @page = page(MainPage).await(timeout:30)
-    @page.exit_to_start
-    @page = page(StartPage).await(timeout:30)
-  else raise("Unknown screen")
-  end
-end
-
-Given ("Я нажимаю кнопку входа в приложение ->") do
-  @page.to_enter_app
-end
-
-Given ("Я оказываюсь на экране входа") do
-  @page = page(EnterPage).await(timeout:30)
-end
-
-Given ("Я оказываюсь на стартовом экране") do
-  @page = page(StartPage).await(timeout:30)
-end
-
-Given ("Я выхожу из приложения") do
-  @page.exit_to_start
+Given("Я оказываюсь на стартовом экране") do
+  pending # Write code here that turns the phrase above into concrete actions
 end
